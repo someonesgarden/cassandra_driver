ad11721a4c26fdc2e921a102aa2f8c54326482fb
Daisuke Nishimura <d@someonesgarden.org>
2016-09-09 21:03:48 +0900

    renamed to main.py

--- cmd.sh
+++ cmd.sh
@@ -4,10 +4,10 @@ set -e
 if [ "$ENV" = 'DEV' ]; then
     echo "$ENV"
     echo "Cassandra Server(Flask)"
-    exec python "/home/uwsgi/app/cassandra_server.py"
+    exec python "/home/uwsgi/app/main.py"
 else
     echo "Cassandra Server(uWSGI)"
-    exec uwsgi --http 0.0.0.0:9090 --wsgi-file /home/uwsgi/app/cassandra_server.py --callable app --stats 0.0.0.0:9191
+    exec uwsgi --http 0.0.0.0:9090 --wsgi-file /home/uwsgi/app/main.py --callable app --stats 0.0.0.0:9191
 fi
 
 
